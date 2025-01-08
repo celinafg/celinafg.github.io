cd28a4c1bc104a55cb847cc20af4b8bb0e0b0763
celina garcia <code@koral.one>
2025-01-09 00:22:51 +0800

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), tailwind(), sitemap()],
+  site: "https://gmail.com",
+});
