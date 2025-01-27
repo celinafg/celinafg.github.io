5dca2063f3ec294ba454d988d2419972f72533b6
celina garcia <code@koral.one>
2025-01-27 22:37:04 +0800

    add types, fix case study toc and styles

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export interface HeadingType {
+  depth: number;
+  slug: string;
+  text: string;
+}
+
+export interface GroupedHeading extends HeadingType {
+  subheadings: GroupedHeading[];
+}
+
+export interface ProjectLayoutProps {
+  image?: string;
+  title: string;
+  description: string;
+  ogType?: string;
+  groupedHeadings: GroupedHeading[];
+}
+
+export interface HeaderProps {
+  currentPath?: string;
+}
+
+export interface NavLink {
+  href: string;
+  label: string;
+}
