86be8929f0179c1bb5b64473f8b8dfaad0cd69e6
celina garcia <code@koral.one>
2025-02-25 16:06:56 +0000

    add index for mdx component export utility

---
+++ src/components/index.tsx
@@ -0,0 +1,8 @@
+import FigmaEmbed from "./FigmaEmbed.astro";
+
+//mdx component exports
+export { default as ImageTextLayout } from "./ImageTextLayout.astro";
+export { default as Divider } from "./Divider.astro";
+export { default as Spacer } from "./Spacer.astro";
+export { default as AudioStoryboard } from "./Audiostoryboard.astro";
+export { default as FigmaEmbed } from "./FigmaEmbed.astro";
