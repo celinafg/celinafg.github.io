1ef36f623872850a9ed1af298299b8a10667f433
celina garcia <code@koral.one>
2025-02-25 16:10:49 +0000

    fix imports of components and naming

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,7 +2,7 @@ import FigmaEmbed from "./FigmaEmbed.astro";
 
 //mdx component exports
 export { default as ImageTextLayout } from "./ImageTextLayout.astro";
-export { default as Divider } from "./Divider.astro";
+export { default as SectionDivider } from "./Divider.astro";
 export { default as Spacer } from "./Spacer.astro";
-export { default as AudioStoryboard } from "./Audiostoryboard.astro";
+export { default as AudioPlayer } from "./Audiostoryboard.astro";
 export { default as FigmaEmbed } from "./FigmaEmbed.astro";
